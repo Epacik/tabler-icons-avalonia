b6c2602e17548a8a47552b4a3dff4bd68940c6b6
Epat <i.am@epat.xyz>
2023-10-16 01:19:02 +0200

    TablerIcons should work with any degault brush

---
+++ packages/icons-avalonia/src/TablerIcons.Avalonia/TablerIconDrawOperation.cs
@@ -0,0 +1,96 @@
+﻿using Avalonia;
+using Avalonia.Media;
+using Avalonia.Platform;
+using Avalonia.Rendering.SceneGraph;
+using Avalonia.Skia;
+using Avalonia.Svg.Skia;
+using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TablerIcons.Avalonia
+{
+    public class TablerIconDrawOperation : ICustomDrawOperation
+    {
+        private readonly Rect _rect;
+        private readonly SvgSource _source;
+        private readonly SKSurface _surface;
+        private readonly SKShader _sKShader;
+        private readonly double _opacity;
+        private bool rendered = false;
+
+        public TablerIconDrawOperation(Rect rect, SvgSource source, SKShader sKShader, double opacity)
+        {
+            _rect = rect;
+            _source = source;
+            _sKShader = sKShader;
+            _opacity = opacity;
+            SKImageInfo imageInfo = new SKImageInfo((int)rect.Width, (int)rect.Height);
+            _surface = SKSurface.Create(imageInfo);
+        }
+
+        public Rect Bounds => _rect;
+
+        public void Dispose()
+        {
+            _surface.Dispose();
+        }
+
+        public bool Equals(ICustomDrawOperation other) => false;
+
+        public bool HitTest(Point p) => false;
+
+        public void Render(ImmediateDrawingContext context)
+        {
+
+            if (_source?.Picture is null)
+            {
+                return;
+            }
+
+            var leaseFeature = context.TryGetFeature<ISkiaSharpApiLeaseFeature>();
+            if (leaseFeature is null)
+            {
+                return;
+            }
+            using (var lease = leaseFeature.Lease())
+            {
+                var canvas = lease?.SkCanvas;
+                if (canvas is null)
+                {
+                    return;
+                }
+                
+                var tempCanvas = _surface.Canvas;
+
+                // throw away previously rendered rect
+                tempCanvas.Clear();
+                tempCanvas.Save();
+
+                // draw a rectangle with selected brush
+                using (var paint = new SKPaint())
+                {
+                    paint.Shader = _sKShader;
+                    paint.IsAntialias = true;
+                    paint.Color = new SKColor(255, 255, 255, (byte)(255 * _opacity));
+                    tempCanvas.DrawRect(_rect.ToSKRect(), paint);
+                }
+
+                // "cut out" the icon out of created rectangle
+                using (var paint = new SKPaint())
+                {
+                    paint.BlendMode = SKBlendMode.DstIn;
+                    paint.IsAntialias = true;
+                    tempCanvas.DrawPicture(_source.Picture, paint);
+                }
+
+                // draw an icon to the screen
+                canvas.Save();
+                canvas.DrawSurface(_surface, new SKPoint(0, 0));
+                canvas.Restore();
+                tempCanvas.Restore();
+            }
+        }
+    }
+}
--- packages/icons-avalonia/src/TablerIcons.Avalonia/TablerIconExtension.cs
+++ packages/icons-avalonia/src/TablerIcons.Avalonia/TablerIconExtension.cs
@@ -4,24 +4,27 @@ using Avalonia.Media;
 
 namespace TablerIcons.Avalonia
 {
-    public sealed class TablerIconSourceExtension : MarkupExtension
+    public sealed class TablerIconExtension : MarkupExtension
     {
-        public TablerIconSourceExtension() { }
-        public TablerIconSourceExtension(Icons icon)
+        public TablerIconExtension() { }
+        public TablerIconExtension(Icons icon)
         {
             Icon = icon;
         }
         public float StrokeWidth { get; set; } = 2;
         public IBrush Brush { get; set; } = Brushes.Black;
         public Icons Icon { get; set; }
-
+        public double Width { get; set; } = 30;
+        public double Height { get; set; } = 30;
         public override object ProvideValue(IServiceProvider serviceProvider)
         {
-            return new TablerIconSource()
+            return new TablerIcon()
             {
                 Icon = Icon,
                 StrokeWidth = StrokeWidth,
                 Brush = Brush,
+                Width = Width,
+                Height = Height,
             };
         }
     }
