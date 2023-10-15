d866f3e7c5aae8daaa0e65aeded6dd1da08b7add
Epat <i.am@epat.xyz>
2023-10-16 01:19:38 +0200

    Improving Previewer for Avalonia

--- packages/icons-avalonia/src/TablerIcons.Avalonia.Preview/Views/MainView.axaml.cs
+++ packages/icons-avalonia/src/TablerIcons.Avalonia.Preview/Views/MainView.axaml.cs
@@ -10,36 +10,5 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
-
-        //var random = new Random();
-        //string values = "0123456789ABCDEF";
-        //Func<char> r = () => values[random.Next(0, values.Length)];
-        //foreach (var value in Enum.GetValues<Icons>())
-        //{
-        //    var color = $"#{r()}{r()}{r()}{r()}{r()}{r()}";
-        //    _icons.Children.Add(new StackPanel()
-        //    {
-        //        Margin = new Thickness(5),
-        //        Children =
-        //        {
-        //            new Image()
-        //            {
-        //                Width = 40,
-        //                Height = 40,
-        //                Source = new TablerIconSource
-        //                {
-        //                    Icon = value,
-        //                    Color = color,
-        //                }
-        //            },
-        //            new TextBlock()
-        //            {
-        //                Text = value.ToString(),
-        //                TextAlignment = TextAlignment.Center
-        //            }
-        //        },
-        //    });
-        //}
     }
-
 }
