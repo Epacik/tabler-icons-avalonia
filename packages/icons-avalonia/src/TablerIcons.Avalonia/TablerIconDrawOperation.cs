6bf424b42aaed832b73da906cb1f31acabad3407
Epat <i.am@epat.xyz>
2023-10-21 14:26:32 +0200

    loading svg data at compile time

--- packages/icons-avalonia/src/TablerIcons.Avalonia/TablerIconDrawOperation.cs
+++ packages/icons-avalonia/src/TablerIcons.Avalonia/TablerIconDrawOperation.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using Avalonia.Media;
+using Avalonia.OpenGL;
 using Avalonia.Platform;
 using Avalonia.Rendering.SceneGraph;
 using Avalonia.Skia;
@@ -9,6 +10,7 @@ using SkiaSharp;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Xml.Linq;
 
@@ -17,13 +19,12 @@ namespace TablerIcons.Avalonia
     internal class TablerIconDrawOperation : ICustomDrawOperation
     {
         private readonly Rect _rect;
-        private readonly IEnumerable<PathData> _pathData;
+        private readonly IEnumerable<ISvgData> _pathData;
         private readonly float _strokeWidth;
         private readonly SKShader _sKShader;
         private readonly double _opacity;
 
-
-        public TablerIconDrawOperation(Rect rect, IEnumerable<PathData> glyph, float strokeWidth, SKShader sKShader, double opacity)
+        public TablerIconDrawOperation(Rect rect, IEnumerable<ISvgData> glyph, float strokeWidth, SKShader sKShader, double opacity)
         {
             _rect = rect;
             _pathData = glyph;
@@ -36,8 +37,6 @@ namespace TablerIcons.Avalonia
 
         public void Dispose()
         {
-            //_surface.Dispose();
-            //_font.Dispose();
         }
 
         public bool Equals(ICustomDrawOperation other) => false;
@@ -48,92 +47,102 @@ namespace TablerIcons.Avalonia
         {
 
             if (_pathData is null)
-            {
                 return;
-            }
 
             var leaseFeature = context.TryGetFeature<ISkiaSharpApiLeaseFeature>();
+
             if (leaseFeature is null)
-            {
                 return;
-            }
-            using (var lease = leaseFeature.Lease())
+
+            using var lease = leaseFeature.Lease();
+
+            var canvas = lease?.SkCanvas;
+            if (canvas is null)
+                return;
+
+
+            canvas.Save();
+
+
+            var defaultStrokeWidth = (_pathData
+                .FirstOrDefault(x => x is DefaultStrokeWidthData)
+                as DefaultStrokeWidthData);
+
+            var strokeMultiplier = (defaultStrokeWidth?.DefaultStrokeWidth ?? 2) / 2f;
+
+            var data = _pathData
+                .Where(x => x is not DefaultStrokeWidthData);
+            foreach (var p in data)
             {
-                var canvas = lease?.SkCanvas;
-                if (canvas is null)
+                using var paint = new SKPaint();
+
+                paint.IsAntialias = true;
+                paint.Shader = _sKShader;
+
+                paint.Color = new SKColor(255, 255, 255);
+                paint.IsStroke = p.IsStroke;
+
+                paint.StrokeWidth = _strokeWidth * strokeMultiplier;
+                paint.StrokeCap = SKStrokeCap.Round;
+                paint.StrokeJoin = SKStrokeJoin.Round;
+
+
+                switch (p)
                 {
-                    return;
+                    case PathData pd:
+                        DrawPath(pd, paint, canvas);
+                        break;
+                    case CircleData cd:
+                        DrawCircle(cd, paint, canvas);
+                        break;
+                    case RectData rd:
+                        DrawRect(rd, paint, canvas);
+                        break;
                 }
-                
-                //var tempCanvas = _surface.Canvas;
-
-                // throw away previously rendered rect
-                //tempCanvas.Clear();
-                //tempCanvas.Save();
-
-
-
-                // draw a rectangle with selected brush
-                //using (var paint = new SKPaint())
-                //{
-                //    paint.Shader = _sKShader;
-                //    paint.IsAntialias = true;
-                //    paint.Color = new SKColor(255, 255, 255, (byte)(255 * _opacity));
-                //    tempCanvas.DrawRect(_rect.ToSKRect(), paint);
-                //}
-
-                canvas.Save();
-
-                // "cut out" the icon out of created rectangle
-
-                foreach (var p in _pathData)
-                {
-                    if (p.Fill == "none" && p.Stroke == "none")
-                        continue;
-
-                    using (var paint = new SKPaint())
-                    {
-                        paint.IsAntialias = true;
-                        paint.Shader = _sKShader;
-
-                        paint.Color = new SKColor(255, 255, 255);
-                        paint.IsStroke = p.Stroke != "none";
-
-                        paint.StrokeWidth = _strokeWidth;
-                        paint.StrokeCap = SKStrokeCap.Round;
-                        paint.StrokeJoin = SKStrokeJoin.Round;
-
-                        var path = SKPath.ParseSvgPathData(p.Data);
-
-                        path.Transform(SKMatrix.CreateScale(
-                            (float)(_rect.Width / 24),
-                            (float)(_rect.Height / 24)));
-
-                        canvas.DrawPath(path, paint);
-                        //tempCanvas.DrawPicture(_source.Picture, paint);
-                        //canvas.DrawText(
-                        //    _glyph,
-                        //    (float)(_rect.Width / 2),
-                        //    (float)(_rect.Height * 0.9f),
-                        //    paint);
-                    }
-                }
-                
-
-                //using (var paint = new SKPaint())
-                //{
-                //    paint.Shader = _sKShader;
-                //    paint.BlendMode = SKBlendMode.Xor;
-                //    paint.IsAntialias = true;
-                //    tempCanvas.DrawRect(_rect.ToSKRect(), paint);
-                //}
-
-                // draw an icon to the screen
-                //canvas.Save();
-                //canvas.DrawSurface(_surface, new SKPoint(0, 0));
-                canvas.Restore();
-                //tempCanvas.Restore();
             }
+
+            canvas.Restore();
+        }
+
+        private void DrawPath(PathData pd, SKPaint paint, SKCanvas canvas)
+        {
+            var path = SKPath.ParseSvgPathData(pd.Data);
+
+            path.Transform(GetScale());
+
+            canvas.DrawPath(path, paint);
+        }
+
+        private void DrawCircle(CircleData cd, SKPaint paint, SKCanvas canvas)
+        {
+            var scale = GetScalingFactor();
+            canvas.DrawCircle(cd.CenterX * scale, cd.CenterY * scale, cd.Radius * scale, paint);
+        }
+
+        private void DrawRect(RectData rd, SKPaint paint, SKCanvas canvas)
+        {
+
+            var scale = GetScalingFactor();
+            canvas.DrawRoundRect(
+                rd.X * scale,
+                rd.Y * scale,
+                rd.Width * scale,
+                rd.Height * scale,
+                rd.Rx * scale,
+                0,
+                paint);
+            throw new NotImplementedException();
+        }
+
+        private SKMatrix GetScale() => SKMatrix.CreateScale(
+                (float)(_rect.Width / 24),
+                (float)(_rect.Height / 24));
+
+        private float GetScalingFactor()
+        {
+            var currentWidth = _rect.Width > _rect.Height ? _rect.Height : _rect.Width;
+
+            return (float)(currentWidth / 24);
         }
     }
 }
